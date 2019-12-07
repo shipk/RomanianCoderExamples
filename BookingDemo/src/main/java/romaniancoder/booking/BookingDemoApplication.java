d0db34622a3ecd1955d53be8cc5e97231f7b287e
shipk <kshipkov@mail.ru>
2019-12-07 11:37:36 +0300

    changes

--- BookingDemo/src/main/java/romaniancoder/booking/BookingDemoApplication.java
+++ BookingDemo/src/main/java/romaniancoder/booking/BookingDemoApplication.java
@@ -3,6 +3,7 @@ package romaniancoder.booking;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServlerInitializer;
 
 @SpringBootApplication
 @EnableJSONDoc
